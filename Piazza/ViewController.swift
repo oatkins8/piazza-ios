c93e23a8ab45de02d8ea2bc8ddfe090d9752c69c
Ollie Atkins <ollieatkins8@outlook.com>
2024-12-29 13:06:39 +0000

    Initial Commit

---
+++ Piazza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piazza
+//
+//  Created by Oliver Atkins on 29/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PiazzaTests/PiazzaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiazzaTests.swift
+//  PiazzaTests
+//
+//  Created by Oliver Atkins on 29/12/2024.
+//
+
+import Testing
+@testable import Piazza
+
+struct PiazzaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
